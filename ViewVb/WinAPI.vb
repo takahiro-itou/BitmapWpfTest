3a385185338853b2e9be9c550befdc2bc25aaa07
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-26 22:03:56 +0900

    API の宣言を追加

--- ViewVb/WinAPI.vb
+++ ViewVb/WinAPI.vb
@@ -3,4 +3,17 @@ Imports System.Runtime.InteropServices
 
 Module WinAPI
 
+Public Const SRCAND As Integer = &H8800C6
+Public Const SRCCOPY As Integer = &HCC0020
+Public Const SRCPAINT As Integer = &HEE0086
+
+<DllImport("gdi32.dll")> _
+Public Function BitBlt(ByVal hDestDC As IntPtr, _
+    ByVal X As Integer, ByVal Y As Integer, _
+    ByVal nWidth As Integer, ByVal nHeight As Integer, _
+    ByVal hSrcDC As IntPtr, _
+    ByVal xSrc As Integer, ByVal ySrc As Integer, _
+    ByVal dwRop As Integer) As Integer
+End Function
+
 End Module
