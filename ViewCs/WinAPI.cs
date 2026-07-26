f987407c61b9379f322e795c21b4992fa75798c2
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-26 22:21:08 +0900

    コンパイルエラーを修正

--- ViewCs/WinAPI.cs
+++ ViewCs/WinAPI.cs
@@ -1,16 +1,21 @@
 
 using System.Runtime.InteropServices;
 
-namespace  ViewCs.WinAPI  {
+namespace  ViewCs  {
+
+public  class  WinAPI
+{
 
 [DllImport("gdi32.dll")]
 public  static  extern  int  BitBlt(
-    IntPtr hDestDC,
+    System.IntPtr hDestDC,
     int X, int Y,
     int nWidth, int nHeight,
-    IntPtr hSrcDC,
+    System.IntPtr hSrcDC,
     int xSrc, int ySrc,
     int dwRop
 );
 
-}   //  End of namespace  ViewCs.WinAPI
+}   //  End class  WinAPI
+
+}   //  End of namespace  ViewCs
