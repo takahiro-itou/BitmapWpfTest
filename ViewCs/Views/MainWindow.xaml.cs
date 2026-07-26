4b864034064618388dc4d2a1e9080fd038a317a0
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-26 09:34:03 +0900

    テンプレートリポジトリの内容をコピー

---
+++ ViewCs/Views/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿
+using System.Windows;
+
+using ViewCs;
+
+
+namespace  ViewCs.Views  {
+
+public  partial class  MainWindow : Window
+{
+
+    //----------------------------------------------------------------
+    /**   デフォルトコンストラクタ。
+    **
+    **/
+    public  MainWindow()
+    {
+        InitializeComponent();
+
+        this.m_taskModel = new Models.SampleModel();
+        this.m_viewModel = new ViewModels.SampleViewModel(this.m_taskModel);
+
+        this.DataContext = this.m_viewModel;
+    }
+
+
+    private Models.SampleModel          m_taskModel;
+    private ViewModels.SampleViewModel  m_viewModel;
+
+}   //  End class  MainWindow
+
+}   //  End of namespace  ViewCs.Views
