332dddae3ecaa75c90d6d49391791edd08ee377d
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-26 23:22:55 +0900

    別リポジトリからコードをコピー
    (1) WinForms 版のイメージ描画の処理

--- ViewVb/Views/MainWindow.xaml.vb
+++ ViewVb/Views/MainWindow.xaml.vb
@@ -24,7 +24,52 @@ Private Sub runCommand()
 ''--------------------------------------------------------------------
 ''    指定したコマンドを実行する。
 ''--------------------------------------------------------------------
+Dim imgCanvas As System.Drawing.Bitmap
+Dim grpCanvas As System.Drawing.Graphics
+Dim imgBuffer As System.Drawing.Bitmap
+Dim grpBuffer As System.Drawing.Graphics
+Dim hDisplayDC As IntPtr
+Dim hDC As IntPtr
+Dim brushBG As SolidBrush
+Dim colorBG As Color
 
+    hDisplayDC = GetDC(IntPtr.Zero)
+
+    imgBuffer = New System.Drawing.Bitmap(200, 100)
+    grpBuffer = System.Drawing.Graphics.FromImage(imgBuffer)
+
+    colorBG = Color.FromArgb(&HFFFEF0BA)
+    brushBG = New SolidBrush(colorBG)
+    ' grpBuffer.FillRectangle(brushBG, grpBuffer.VisibleClipBounds)
+    grpBuffer.FillRectangle(brushBG, 0, 0, 200, 100)
+
+    hDC = grpBuffer.GetHdc()
+    BitBlt(hDC, 8, 8, 184, 84, hDisplayDC,
+            Screen.PrimaryScreen.Bounds.Width - 184,
+            Screen.PrimaryScreen.Bounds.Height - 84,
+            SRCCOPY)
+    grpBuffer.ReleaseHdc(hDC)
+
+    grpBuffer.DrawRectangle(Pens.Yellow, 50, 30, 100, 60)
+    grpBuffer.DrawPie(Pens.Red, 60, 10, 80, 80, 30, 300)
+    grpBuffer.Dispose()
+
+    imgCanvas = New System.Drawing.Bitmap(300, 300)
+    grpCanvas = System.Drawing.Graphics.FromImage(imgCanvas)
+
+    colorBG = Color.FromArgb(&H800000FF)
+    brushBG = New SolidBrush(colorBG)
+    ' grpCanvas.FillRectangle(brushBG, grpCanvas.VisibleClipBounds)
+    grpCanvas.FillRectangle(brushBG, 0, 0, 300, 300)
+
+    hDC = grpCanvas.GetHdc()
+    BitBlt(hDC, 8, 8, 284, 284, hDisplayDC, 0, 0, SRCCOPY)
+    grpCanvas.ReleaseHdc(hDC)
+
+    ReleaseDC(IntPtr.Zero, hDisplayDC)
+
+    grpCanvas.DrawImage(imgBuffer, 50, 100, 200, 100)
+    grpCanvas.Dispose()
 End Sub
 
 
