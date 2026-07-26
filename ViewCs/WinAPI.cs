5d9139d7c90f61fcc2bc9067e324a2163c24753b
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-07-26 22:15:01 +0900

    API の宣言を追加

--- ViewCs/WinAPI.cs
+++ ViewCs/WinAPI.cs
@@ -3,4 +3,14 @@ using System.Runtime.InteropServices;
 
 namespace  ViewCs.WinAPI  {
 
+[DllImport("gdi32.dll")]
+public  static  extern  int  BitBlt(
+    IntPtr hDestDC,
+    int X, int Y,
+    int nWidth, int nHeight,
+    IntPtr hSrcDC,
+    int xSrc, int ySrc,
+    int dwRop
+);
+
 }   //  End of namespace  ViewCs.WinAPI
